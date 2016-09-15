698c0281211f9b50cc3a9003fddebe82a7480a98
Dominic Serrano <dmsrojo@gmail.com>
2016-09-15 12:58:54 -0500

    Can now render home post view with username

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,27 @@
 const { Router } = require('express');
 const router = Router();
 
+/////////////////////////////////////////
 router.get('/', (req,res) => {
-    res.render('home')
-})
+    res.render('login')
+});
 
+router.get('/posts', (req, res) => {
+  //On redirection to '/posts' render postView with userName
+  res.render('postView', {userName: req.session.username});
+});
+/////////////////////////////////////////
+
+
+/////////////////////////////////////////
 router.post('/', (req, res) => {
 
   //Stores the username in a local session on sign in
   req.session.username = req.body.username;
 
-  res.render('');
+  res.redirect('/posts');
 
 });
+/////////////////////////////////////////
 
 module.exports = router;
