575fc34ac7e47ac17d0befade79641348a633418
Dominic Serrano <dmsrojo@gmail.com>
2016-09-15 12:40:56 -0500

    Set up home page with form

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const connect = require('./database');
-const router = require('./routes')
+const router = require('./routes/routes');
+const session = require('express-session');
+
 
 app.set('view engine', 'pug');
 
-app.use(router)
+/////////////////////////////////////////
+app.use(session({secret: 'username'}));
+
+app.use(router);
 
 connect()
     .then(() => {
